eaf3a2ac047323d89dfd1067db6f1ed2bc4f5552
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-01-18 12:31:56 +0300

    feat: поля формы заполнить значениями со страницы

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let editButton = document.querySelector('.profile__edit-button');
+let profile = document.querySelector('.profile');
+let editButton = profile.querySelector('.profile__edit-button');
+let profileName = profile.querySelector('.profile__name');
+let profileMetier = profile.querySelector('.profile__metier');
 let popup = document.querySelector('.popup');
 let popupClose = popup.querySelector('.popup__close');
+let popupInputText = popup.querySelectorAll('.popup__input-text');
 
 function showPopup() {
   popup.classList.add('popup_opened');
+  // console.log(popupInputText);
+  // console.log(profileName.textContent);
+  // console.log(profileMetier.textContent);
+  popupInputText[0].value = profileName.textContent;
+  popupInputText[1].value = profileMetier.textContent;
 }
 
 editButton.addEventListener('click', showPopup);
