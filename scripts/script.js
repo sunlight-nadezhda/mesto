e60fd1b53f69bffb78dc4219c2ff90e368388831
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-02-04 20:11:46 +0300

    refactor: добавить готовый массив для заполнения карточек

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,33 @@ let formElement = popup.querySelector('.popup__container');
 let nameInput = formElement.querySelector('.popup__input-text_type_name');
 let jobInput = formElement.querySelector('.popup__input-text_type_metier');
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 const togglePopup = (event) => {
   event.preventDefault();
   popup.classList.toggle('popup_opened');
