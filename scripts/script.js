70b5b48dcf5ae95df657854428517572c03146bf
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-02-04 22:10:11 +0300

    feat: добавить отображение одной карточки

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
-let profile = document.querySelector('.profile');
-let editButton = profile.querySelector('.profile__edit-button');
-let profileName = profile.querySelector('.profile__name');
-let profileMetier = profile.querySelector('.profile__metier');
+const profile = document.querySelector('.profile');
+const editButton = profile.querySelector('.profile__edit-button');
+const profileName = profile.querySelector('.profile__name');
+const profileMetier = profile.querySelector('.profile__metier');
+const elementsList = document.querySelector('.elements');
 
-let popup = document.querySelector('.popup');
-let popupClose = popup.querySelector('.popup__close-button');
-let formElement = popup.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__input-text_type_name');
-let jobInput = formElement.querySelector('.popup__input-text_type_metier');
+const popup = document.querySelector('.popup');
+const popupClose = popup.querySelector('.popup__close-button');
+const formElement = popup.querySelector('.popup__container');
+const nameInput = formElement.querySelector('.popup__input-text_type_name');
+const jobInput = formElement.querySelector('.popup__input-text_type_metier');
 
 const initialCards = [
   {
@@ -36,16 +37,29 @@ const initialCards = [
   }
 ];
 
+const addElementCard = (nameValue, linkValue) => {
+  const elementTemplate = document.querySelector('#element').content;
+  const elementListItem = elementTemplate.querySelector('.element').cloneNode(true);
+
+  elementListItem.querySelector('.element__image').setAttribute('src', linkValue);
+  elementListItem.querySelector('.element__image').setAttribute('alt', nameValue);
+  elementListItem.querySelector('.element__title').textContent = nameValue;
+
+  elementsList.append(elementListItem);
+};
+
+addElementCard('Архыз', 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg');
+
 const togglePopup = event => {
   event.preventDefault();
   popup.classList.toggle('popup_opened');
-}
+};
 
 const showPopup = event => {
   togglePopup(event);
   nameInput.value = profileName.textContent;
   jobInput.value = profileMetier.textContent;
-}
+};
 
 const closePopup = event => {
   if (event.target === event.currentTarget) {
@@ -57,7 +71,7 @@ const handleFormSubmit = event => {
   profileName.textContent = nameInput.value;
   profileMetier.textContent = jobInput.value;
   togglePopup(event);
-}
+};
 
 editButton.addEventListener('click', showPopup);
 popupClose.addEventListener('click', togglePopup);
