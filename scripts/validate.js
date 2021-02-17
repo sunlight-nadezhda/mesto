cd8addfc279d36dbb5d76d584b3ddf1748ac7460
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-02-17 16:01:56 +0300

    feat: add script to validate and find some elements

---
+++ scripts/validate.js
@@ -0,0 +1,3 @@
+const formElement = document.querySelector('.form');
+const formInput = formElement.querySelector('.popup__input-text');
+const formError = formElement.querySelector(`.${formInput.id}-error`);
