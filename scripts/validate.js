e501967d7641de4c2d8822de238c753c97bd5971
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-02-17 16:56:18 +0300

    style: add style for error and placeholder

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,3 +1,10 @@
 const formElement = document.querySelector('.form');
-const formInput = formElement.querySelector('.popup__input-text');
+const formInput = formElement.querySelector('.popup__input');
 const formError = formElement.querySelector(`.${formInput.id}-error`);
+
+const showError = () => {
+  formInput.classList.add('popup__input_type_error');
+  formError.classList.add('popup__input-error_active');
+};
+
+showError();
