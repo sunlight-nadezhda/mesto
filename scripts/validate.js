16ce80256c1e7a58115d678d0b69e23f689c1b05
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-02-17 17:11:24 +0300

    feat: validate a field

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,9 +2,30 @@ const formElement = document.querySelector('.form');
 const formInput = formElement.querySelector('.popup__input');
 const formError = formElement.querySelector(`.${formInput.id}-error`);
 
-const showError = () => {
-  formInput.classList.add('popup__input_type_error');
+const showError = (input, errorMessage) => {
+  input.classList.add('popup__input_type_error');
+  formError.textContent = errorMessage;
   formError.classList.add('popup__input-error_active');
 };
 
-showError();
+const hideError = (input) => {
+  input.classList.remove('popup__input_type_error');
+  formError.classList.remove('popup__input-error_active');
+  formError.textContent = '';
+};
+
+const checkInputValidity = () => {
+  if (!formInput.validity.valid) {
+    showError(formInput, formInput.validationMessage);
+  } else {
+    hideError(formInput);
+  }
+};
+
+formElement.addEventListener('submit', evt => {
+  evt.preventDefault();
+});
+
+formInput.addEventListener('input', () => {
+  checkInputValidity();
+});
