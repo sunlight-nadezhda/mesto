36f986e1ab191ef5f57c6cb5997a097498922193
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-03-25 09:51:18 +0300

    feat: defined a class constructor

---
+++ components/Section.js
@@ -0,0 +1,7 @@
+export default class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._renderedItems = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+}
