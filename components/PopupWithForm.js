de8a0b791801c252d9a67c84360f9f95645f94c5
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-03-25 11:48:25 +0300

    feat: add a private method that collects data from all form fields

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -3,4 +3,12 @@ export default class PopupWithForm extends Popup {
     super(popupSelector);
     this._submiter = submiter;
   }
+
+  _getInputValues() {
+    const formElement = this._popup.querySelector('.popup__form');
+    const inputList = formElement.querySelectorAll('.popup__input');
+    const inputValues = {};
+    inputList.forEach(input => inputValues[input.name] = input.value);
+    return inputValues;
+  }
 }
