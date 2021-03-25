7f4b10bb6719ca0b2497b91349a5ffec1cea3571
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-03-25 12:43:07 +0300

    feat: create class with constructor

---
+++ components/UserInfo.js
@@ -0,0 +1,6 @@
+export default class UserInfo {
+  constuctor({ nameElementSelector, metierElementSelector }) {
+    this._nameElementSelector = nameElementSelector;
+    this._metierElementSelector = metierElementSelector;
+  }
+}
