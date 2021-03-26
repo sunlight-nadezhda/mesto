055cdc4157359cf1c4bbb86f0cd114ade210cfd1
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-03-26 11:36:07 +0300

    feat: set up two builds: build and dev

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: { main: './src/pages/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main2.js',
+    publicPath: ''
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+    open: true
+  }
+}
